2613fccc2461f852a7cb383b934dd821b159fe1c
martyloves-alt <martyloves@gmail.com>
2026-06-29 23:58:03 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'screens/studio_screen.dart';
+import 'services/notification_service.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await NotificationService.init();
   runApp(const VizionAIApp());
 }
 
