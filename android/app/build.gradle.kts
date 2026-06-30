ffc735c7744a67469bbb76650dea9da27fd5d960
martyloves-alt <martyloves@gmail.com>
2026-06-30 23:25:27 +0100

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -10,7 +10,6 @@ android {
     ndkVersion = "27.0.12077973"
 
     compileOptions {
-        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
@@ -32,10 +31,6 @@ android {
             signingConfig = signingConfigs.getByName("debug")
         }
     }
-}
-
-dependencies {
-    coreLibraryDesugaring("com.android.tools.build:desugaring:2.0.4")
 }
 
 flutter {
